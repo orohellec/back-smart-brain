cf5b7220cd68acdd59970c6a46812062406d27bf
Olivier <orohellec44@gmail.com>
2019-03-29 14:44:09 +0100

    resolve a small mistake

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('it is working') })
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
@@ -35,6 +35,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
-app.listen(3000, () => {
-  console.log('app is runnig on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is runnig on port ${process.env.PORT}`);
 })
