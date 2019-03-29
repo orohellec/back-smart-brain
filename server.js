f44e1d96319daba191d1e702ae7f1ef80df9d963
Olivier <orohellec44@gmail.com>
2019-03-29 15:44:25 +0100

    database on heroku

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const image = require('./controllers/image');
 const db = knex ({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'olivier',
-    password : '',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
